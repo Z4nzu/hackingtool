1d63c3fde8bb9e67ed82c019fdd14a682fa3faa6
Mr.Z4nzu <hardikzinzuvadiya7@gmail.com>
2020-04-11 15:00:14 +0530

    FirstTime Upload

---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+clear
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+
+echo -e "${CYAN} "
+echo ""
+echo "   ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄           ███      ▄██████▄   ▄██████▄   ▄█       ";
+echo "  ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███      ▀█████████▄ ███    ███ ███    ███ ███       ";
+echo "  ███    ███     ███    ███ ███    █▀    ███▐██▀   ███▌ ███   ███   ███    █▀          ▀███▀▀██ ███    ███ ███    ███ ███       ";
+echo " ▄███▄▄▄▄███▄▄   ███    ███ ███         ▄█████▀    ███▌ ███   ███  ▄███                 ███   ▀ ███    ███ ███    ███ ███       ";
+echo "▀▀███▀▀▀▀███▀  ▀███████████ ███        ▀▀█████▄    ███▌ ███   ███ ▀▀███ ████▄           ███     ███    ███ ███    ███ ███       ";
+echo "  ███    ███     ███    ███ ███    █▄    ███▐██▄   ███  ███   ███   ███    ███          ███     ███    ███ ███    ███ ███       ";
+echo "  ███    ███     ███    ███ ███    ███   ███ ▀███▄ ███  ███   ███   ███    ███          ███     ███    ███ ███    ███ ███▌    ▄ ";
+echo "  ███    █▀      ███    █▀  ████████▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀          ▄████▀    ▀██████▀   ▀██████▀  █████▄▄██ ";
+echo "                                         ▀                                                                            ▀         ";                         
+echo -e "${YELLOW}     【T】【h】【i】【s】 【T】【o】【o】【l】 【C】【r】【e】【a】【t】【e】【d】 【B】【y】 【Z】【4】【n】【z】【u】 "
+
+echo -e "${BLUE}                                    Github.com/Z4nzu ${NC}"
+
+echo -e "${RED}                                   [!] This Tool Must Run As ROOT [!]${NC}"
+echo -e "${WHITE}[>] Press ENTER to Install Hacking Tool, CTRL+C to Abort.${NC}"
+read INPUT
+echo ""
+
+if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
+    INSTALL_DIR="$PREFIX/usr/share/doc/hackingtool"
+    BIN_DIR="$PREFIX/usr/bin/"
+    pkg install -y git python3
+else
+    INSTALL_DIR="/usr/share/doc/hackingtool"
+    BIN_DIR="/usr/bin/"
+fi
+
+echo "[✔] Checking directories...";
+if [ -d "$INSTALL_DIR" ]; then
+    echo "[!] A Directory hackingtool Was Found.. Do You Want To Replace It ? [y/n]:" ;
+    read mama
+    if [ "$mama" = "y" ]; then
+        rm -R "$INSTALL_DIR"
+    else
+        exit
+    fi
+fi
+
+echo "[✔] Installing ...";
+echo "";
+git clone https://github.com/Z4nzu/hackingtool.git "$INSTALL_DIR";
+echo "#!/bin/bash
+python $INSTALL_DIR/hackingtool.py" '${1+"$@"}' > hackingtool;
+chmod +x hackingtool;
+sudo cp hackingtool /usr/bin/;
+rm hacktronian;
+
+
+if [ -d "$INSTALL_DIR" ] ;
+then
+    echo "";
+    echo "[✔] Successfuly Installed !!! [✔]";
+    echo "";
+    echo "[✔]========================================================================[✔]";
+    echo "[✔] ✔✔✔ All Is Done!! you can execute tool by typing hackingtool !! ✔✔✔ [✔]";
+    echo "[✔]========================================================================[✔]";
+    echo "";
+else
+    echo "[✘] Installation Failed !!! [✘]";
+    exit
+fi
---
+++ update.sh
@@ -0,0 +1,53 @@
+echo "███████╗██╗  ██╗███╗   ██╗███████╗██╗   ██╗    ";
+echo "╚══███╔╝██║  ██║████╗  ██║╚══███╔╝██║   ██║    ";
+echo "  ███╔╝ ███████║██╔██╗ ██║  ███╔╝ ██║   ██║    ";
+echo " ███╔╝  ╚════██║██║╚██╗██║ ███╔╝  ██║   ██║    ";
+echo "███████╗     ██║██║ ╚████║███████╗╚██████╔╝    ";
+echo "╚══════╝     ╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝     ";
+echo "                                               ";
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/hackingtool/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/hackingtool
+
+clear
+
+mkdir hackingtool
+
+clear
+
+cd hackingtool
+
+clear
+
+git clone https://github.com/Z4nzu/hackingtool.git
+
+clear
+
+cd hackingtool
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
