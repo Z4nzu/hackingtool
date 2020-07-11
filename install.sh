e7a1e3d6f5b20c5fd859adaf49d158a5f126e033
Mr.Z4nzu <hardikzinzuvadiya7@gmail.com>
2020-07-11 17:49:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,17 @@ clear
 
 BLACK='\e[30m'
 RED='\e[31m'
-GREEN='\e[32m'
+GREEN='\e[92m'
 YELLOW='\e[33m'
+ORANGE='\e[93m'
 BLUE='\e[34m'
 PURPLE='\e[35m'
 CYAN='\e[36m'
 WHITE='\e[37m'
 NC='\e[0m'
+purpal='\033[35m'
 
-echo -e "${CYAN} "
+echo -e "${ORANGE} "
 echo ""
 echo "   ▄█    █▄       ▄████████  ▄████████    ▄█   ▄█▄  ▄█  ███▄▄▄▄      ▄██████▄           ███      ▄██████▄   ▄██████▄   ▄█       ";
 echo "  ███    ███     ███    ███ ███    ███   ███ ▄███▀ ███  ███▀▀▀██▄   ███    ███      ▀█████████▄ ███    ███ ███    ███ ███       ";
@@ -23,53 +25,72 @@ echo "  ███    ███     ███    ███ ███    ███
 echo "  ███    █▀      ███    █▀  ████████▀    ███   ▀█▀ █▀    ▀█   █▀    ████████▀          ▄████▀    ▀██████▀   ▀██████▀  █████▄▄██ ";
 echo "                                         ▀                                                                            ▀         ";                         
 
-echo -e "${BLUE}                                    Github.com/Z4nzu ${NC}"
+echo -e "${BLUE}                                    https://github.com/Z4nzu/hackingtool ${NC}"
 
 echo -e "${RED}                                   [!] This Tool Must Run As ROOT [!]${NC}"
-echo -e "${WHITE}[>] Press ENTER to Install Hacking Tool, CTRL+C to Abort.${NC}"
-read INPUT
 echo ""
+echo -e ${CYAN}              "Select Best Option : "
+echo ""
+echo -e "${WHITE}              [1] Kali Linux / Parrot-Os "
+echo -e "${WHITE}              [0] Exit "
+echo -n -e "Z4nzu >> "
+read choice
+INSTALL_DIR="/usr/share/doc/hackingtool"
+BIN_DIR="/usr/bin/"
+if [ $choice == 1 ]; then 
+	echo "[*] Checking Internet Connection .."
+	wget -q --tries=10 --timeout=20 --spider http://google.com
+	if [[ $? -eq 0 ]]; then
+	    echo -e ${BLUE}"[✔] Loading ... "
+	    sudo apt-get update && apt-get upgrade 
+	    sudo apt-get install python-pip
+	    echo "[✔] Checking directories..."
+	    if [ -d "$INSTALL_DIR" ]; then
+	        echo "[!] A Directory hackingtool Was Found.. Do You Want To Replace It ? [y/n]:" ;
+	        read input
+	        if [ "$input" = "y" ]; then
+	            rm -R "$INSTALL_DIR"
+	        else
+	            exit
+	        fi
+	    fi
+    		echo "[✔] Installing ...";
+		echo "";
+		git clone https://github.com/Z4nzu/hackingtool.git "$INSTALL_DIR";
+		echo "#!/bin/bash
+		python3 $INSTALL_DIR/hackingtool.py" '${1+"$@"}' > hackingtool;
+		sudo chmod +x hackingtool;
+		sudo cp hackingtool /usr/bin/;
+		rm hackingtool;
+		echo ""; 
+		echo "[✔] Trying to installing Requirements ..."
+		sudo pip3 install lolcat
+		sudo apt-get install -y figlet
+		sudo pip3 install boxes
+		sudo pip3 install flask
+		sudo pip3 install requests
+	else 
+		echo -e $RED "Please Check Your Internet Connection ..!!"
+	fi
 
-if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
-    INSTALL_DIR="$PREFIX/usr/share/doc/hackingtool"
-    BIN_DIR="$PREFIX/usr/bin/"
-    pkg install -y git python3
-else
-    INSTALL_DIR="/usr/share/doc/hackingtool"
-    BIN_DIR="/usr/bin/"
-fi
-
-echo "[✔] Checking directories...";
-if [ -d "$INSTALL_DIR" ]; then
-    echo "[!] A Directory hackingtool Was Found.. Do You Want To Replace It ? [y/n]:" ;
-    read mama
-    if [ "$mama" = "y" ]; then
-        rm -R "$INSTALL_DIR"
+    if [ -d "$INSTALL_DIR" ]; then
+        echo "";
+        echo "[✔] Successfuly Installed !!! ";
+        echo "";
+        echo "";
+        echo -e $ORANGE "		[+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]"
+        echo 		"		[+]						      		[+]"
+        echo -e $ORANGE  "		[+]     ✔✔✔ Now Just Type In Terminal (hackingtool) ✔✔✔ 	[+]"
+        echo 		"		[+]						      		[+]"
+        echo -e $ORANGE "		[+]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[+]"
     else
+        echo "[✘] Installation Failed !!! [✘]";
         exit
     fi
-fi
-
-echo "[✔] Installing ...";
-echo "";
-git clone https://github.com/Z4nzu/hackingtool.git "$INSTALL_DIR";
-echo "#!/bin/bash
-python3 $INSTALL_DIR/hackingtool.py" '${1+"$@"}' > hackingtool;
-chmod +x hackingtool;
-sudo cp hackingtool /usr/bin/;
-rm hackingtool;
-
-
-if [ -d "$INSTALL_DIR" ] ;
+elif [ $choice -eq 0 ];
 then
-    echo "";
-    echo "[✔] Successfuly Installed !!! [✔]";
-    echo "";
-    echo "[✔]========================================================================[✔]";
-    echo "[✔] ✔✔✔ All Is Done!! you can execute tool by typing hackingtool !! ✔✔✔ [✔]";
-    echo "[✔]========================================================================[✔]";
-    echo "";
-else
-    echo "[✘] Installation Failed !!! [✘]";
+    echo -e $RED "[✘] THank Y0u !! [✘] "
     exit
+else 
+    echo -e $RED "[!] Select Valid Option [!]"
 fi
