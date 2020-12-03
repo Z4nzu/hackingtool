8b963b9448ab290d90cb6263843091cf650cf08a
d3v53c <shitfi63@gmail.com>
2020-12-03 01:36:53 -0800

    change in mix tools

--- tools/others/mix_tools.py
+++ tools/others/mix_tools.py
@@ -8,10 +8,12 @@ class TerminalMultiplexer(HackingTool):
     DESCRIPTION = "Terminal Multiplexer is a tiling terminal emulator that " \
                   "allows us to open \n several terminal sessions inside one " \
                   "single window."
-    INSTALL_COMMANDS = ["sudo apt-get install tilix"]
+    INSTALL_COMMANDS = [
+        dict(cmd="sudo apt-get install tilix"),
+    ]
 
     def __init__(self):
-        super(TerminalMultiplexer, self).__init__(runnable = False)
+        super(TerminalMultiplexer, self).__init__(runnable=False)
 
 
 class MixTools(HackingToolsCollection):
