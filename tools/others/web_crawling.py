ea900ab6eaa1c52988e7d8fced9499a76974a17e
d3v53c <shitfi63@gmail.com>
2020-12-03 01:59:36 -0800

    change in web crawler

--- tools/others/web_crawling.py
+++ tools/others/web_crawling.py
@@ -6,11 +6,13 @@ from core import HackingToolsCollection
 class GoSpider(HackingTool):
     TITLE = "Gospider"
     DESCRIPTION = "Gospider - Fast web spider written in Go"
-    INSTALL_COMMANDS = ["sudo go get -u github.com/jaeles-project/gospider"]
+    INSTALL_COMMANDS = [
+        dict(cmd="sudo go get -u github.com/jaeles-project/gospider"),
+    ]
     PROJECT_URL = "https://github.com/jaeles-project/gospider"
 
     def __init__(self):
-        super(GoSpider, self).__init__(runnable = False)
+        super(GoSpider, self).__init__(runnable=False)
 
 
 class WebCrawlingTools(HackingToolsCollection):
