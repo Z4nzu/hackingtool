f2c21b9ed4ce30fef8388057a2ca84b4cdbc8425
d3v53c <shitfi63@gmail.com>
2020-12-03 01:34:40 -0800

    change in homograph attacks

--- tools/others/homograph_attacks.py
+++ tools/others/homograph_attacks.py
@@ -7,8 +7,12 @@ class EvilURL(HackingTool):
     TITLE = "EvilURL"
     DESCRIPTION = "Generate unicode evil domains for IDN Homograph Attack " \
                   "and detect them."
-    INSTALL_COMMANDS = ["git clone https://github.com/UndeadSec/EvilURL.git"]
-    RUN_COMMANDS = ["cd EvilURL;python3 evilurl.py"]
+    INSTALL_COMMANDS = [
+        dict(cmd="git clone https://github.com/UndeadSec/EvilURL.git"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="python3 evilurl.py", cwd="EvilURL"),
+    ]
     PROJECT_URL = "https://github.com/UndeadSec/EvilURL"
 
 
