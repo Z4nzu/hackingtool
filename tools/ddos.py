b6c632512f2b398b1f63841aaa34faf88409b3f4
d3v53c <shitfi63@gmail.com>
2020-12-03 02:12:24 -0800

    change in ddos

--- tools/ddos.py
+++ tools/ddos.py
@@ -4,17 +4,20 @@ import subprocess
 
 from core import HackingTool
 from core import HackingToolsCollection
+from core.utils import run_command
 
 
 class SlowLoris(HackingTool):
     TITLE = "SlowLoris"
     DESCRIPTION = "Slowloris is basically an HTTP Denial of Service attack." \
                   "It send lots of HTTP Request"
-    INSTALL_COMMANDS = ["sudo pip install slowloris"]
+    INSTALL_COMMANDS = [
+        dict(cmd="sudo pip install slowloris"),
+    ]
 
     def run(self):
         target_site = input("Enter Target Site:- ")
-        subprocess.run(["slowloris", target_site])
+        run_command(f"slowloris {target_site}")
 
 
 class Asyncrone(HackingTool):
@@ -23,8 +26,9 @@ class Asyncrone(HackingTool):
                   "DDoS Weapon.\nDisable the destination system by sending a " \
                   "SYN packet intensively to the destination."
     INSTALL_COMMANDS = [
-        "git clone https://github.com/fatih4842/aSYNcrone.git",
-        "cd aSYNcrone;sudo gcc aSYNcrone.c -o aSYNcrone -lpthread"
+        dict(cmd="git clone https://github.com/fatih4842/aSYNcrone.git"),
+        dict(cmd="sudo gcc aSYNcrone.c -o aSYNcrone -lpthread",
+             cwd="aSYNcrone"),
     ]
     PROJECT_URL = "https://github.com/fatihsnsy/aSYNcrone"
 
@@ -32,9 +36,10 @@ class Asyncrone(HackingTool):
         source_port = input("Enter Source Port >> ")
         target_ip = input("Enter Target IP >> ")
         target_port = input("Enter Target port >> ")
-        os.system("cd aSYNcrone;")
-        subprocess.run([
-            "sudo", "./aSYNcrone", source_port, target_ip, target_port, 1000])
+        run_command(
+            f"sudo ./aSYNcrone {source_port} {target_ip} {target_port} 1000",
+            cwd="aSYNcrone",
+        )
 
 
 class UFONet(HackingTool):
@@ -44,10 +49,12 @@ class UFONet(HackingTool):
                   "DDoS attacks\n\b " \
                   "More Usage Visit"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/epsylon/ufonet.git",
-        "cd ufonet;sudo python setup.py install"
+        dict(cmd="sudo git clone https://github.com/epsylon/ufonet.git"),
+        dict(cmd="sudo python setup.py install", cwd="ufonet"),
     ]
-    RUN_COMMANDS = ["sudo ./ufonet --gui"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo ./ufonet --gui"),
+    ]
     PROJECT_URL = "https://github.com/epsylon/ufonet"
 
 
@@ -56,22 +63,17 @@ class GoldenEye(HackingTool):
     DESCRIPTION = "GoldenEye is an python3 app for SECURITY TESTING PURPOSES ONLY!\n" \
                   "GoldenEye is a HTTP DoS Test Tool."
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/jseidl/GoldenEye.git;"
-        "chmod -R 755 GoldenEye"
+        dict(cmd="sudo git clone https://github.com/jseidl/GoldenEye.git"),
+        dict(cmd="chmod -R 755 GoldenEye"),
     ]
     PROJECT_URL = "https://github.com/jseidl/GoldenEye"
 
     def run(self):
-        os.system("cd GoldenEye ;sudo ./goldeneye.py")
+        run_command("sudo ./goldeneye.py", cwd="GoldenEye")
         print("\033[96m Go to Directory \n "
               "[*] USAGE: ./goldeneye.py <url> [OPTIONS]")
 
 
 class DDOSTools(HackingToolsCollection):
     TITLE = "DDOS Attack Tools"
-    TOOLS = [
-        SlowLoris(),
-        Asyncrone(),
-        UFONet(),
-        GoldenEye()
-    ]
+    TOOLS = [SlowLoris(), Asyncrone(), UFONet(), GoldenEye()]
