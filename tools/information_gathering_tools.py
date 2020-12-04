3b63f0f725eb45ab58a48d04b4d9e9ecb216b896
d3v53c <shitfi63@gmail.com>
2020-12-04 11:53:52 -0800

    changes in information gathering

--- tools/information_gathering_tools.py
+++ tools/information_gathering_tools.py
@@ -7,19 +7,23 @@ import webbrowser
 from core import HackingTool
 from core import HackingToolsCollection
 from core import clear_screen
+from core.utils import run_command
 
 
 class NMAP(HackingTool):
     TITLE = "Network Map (nmap)"
     DESCRIPTION = "Free and open source utility for network discovery and security auditing"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/nmap/nmap.git",
-        "sudo chmod -R 755 nmap && cd nmap && sudo ./configure && make && sudo make install"
+        dict(cmd="sudo git clone https://github.com/nmap/nmap.git"),
+        dict(cmd="sudo chmod -R 755 nmap", cwd="nmap"),
+        dict(cmd="sudo ./configure", cwd="nmap"),
+        dict(cmd="make", cwd="nmap"),
+        dict(cmd="sudo make install", cwd="nmap"),
     ]
     PROJECT_URL = "https://github.com/nmap/nmap"
 
     def __init__(self):
-        super(NMAP, self).__init__(runnable = False)
+        super(NMAP, self).__init__(runnable=False)
 
 
 class Dracnmap(HackingTool):
@@ -27,32 +31,32 @@ class Dracnmap(HackingTool):
     DESCRIPTION = "Dracnmap is an open source program which is using to \n" \
                   "exploit the network and gathering information with nmap help."
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/Screetsec/Dracnmap.git",
-        "cd Dracnmap && chmod +x Dracnmap.sh"
+        dict(cmd="sudo git clone https://github.com/Screetsec/Dracnmap.git"),
+        dict(cmd="chmod +x Dracnmap.sh", cwd="Dracnmap")
     ]
     PROJECT_URL = "https://github.com/Screetsec/Dracnmap"
 
     def __init__(self):
-        super(Dracnmap, self).__init__(runnable = False)
+        super(Dracnmap, self).__init__(runnable=False)
 
 
 class PortScan(HackingTool):
     TITLE = "Port scanning"
 
     def __init__(self):
-        super(PortScan, self).__init__(installable = False)
+        super(PortScan, self).__init__(installable=False)
 
     def run(self):
         clear_screen()
         target = input('Select a Target IP: ')
-        subprocess.run(["sudo", "nmap", "-O", "-Pn", target])
+        run_command(f"sudo nmap -O -Pn {target}")
 
 
 class Host2IP(HackingTool):
     TITLE = "Host to IP "
 
     def __init__(self):
-        super(Host2IP, self).__init__(installable = False)
+        super(Host2IP, self).__init__(installable=False)
 
     def run(self):
         clear_screen()
@@ -66,10 +70,12 @@ class XeroSploit(HackingTool):
     DESCRIPTION = "Xerosploit is a penetration testing toolkit whose goal is to perform\n" \
                   "man-in-the-middle attacks for testing purposes"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/LionSec/xerosploit.git",
-        "cd xerosploit && sudo python install.py"
+        dict(cmd="git clone https://github.com/LionSec/xerosploit.git"),
+        dict(cmd="sudo python install.py", cwd="xerosploit")
     ]
-    RUN_COMMANDS = ["sudo xerosploit"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo xerosploit"),
+    ]
     PROJECT_URL = "https://github.com/LionSec/xerosploit"
 
 
@@ -77,8 +83,11 @@ class RedHawk(HackingTool):
     TITLE = "RED HAWK (All In One Scanning)"
     DESCRIPTION = "All in one tool for Information Gathering and Vulnerability Scanning."
     INSTALL_COMMANDS = [
-        "git clone https://github.com/Tuhinshubhra/RED_HAWK.git"]
-    RUN_COMMANDS = ["cd RED_HAWK;php rhawk.php"]
+        dict(cmd="git clone https://github.com/Tuhinshubhra/RED_HAWK.git")
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="php rhawk.php", cwd="RED_HAWK"),
+    ]
     PROJECT_URL = "https://github.com/Tuhinshubhra/RED_HAWK"
 
 
@@ -89,13 +98,14 @@ class ReconSpider(HackingTool):
                   "Websites, Organizations and find out information from" \
                   " different sources.\n"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/bhavsec/reconspider.git",
-        "sudo apt install python3 python3-pip && cd reconspider && sudo python3 setup.py install"
+        dict(cmd="sudo git clone https://github.com/bhavsec/reconspider.git"),
+        dict(cmd="sudo apt install python3 python3-pip"),
+        dict(cmd="sudo python3 setup.py install", cwd="reconspider"),
     ]
     PROJECT_URL = "https://github.com/bhavsec/reconspider"
 
     def __init__(self):
-        super(ReconSpider, self).__init__(runnable = False)
+        super(ReconSpider, self).__init__(runnable=False)
 
 
 class IsItDown(HackingTool):
@@ -103,8 +113,9 @@ class IsItDown(HackingTool):
     DESCRIPTION = "Check Website Is Online or Not"
 
     def __init__(self):
-        super(IsItDown, self).__init__(
-            [('Open', self.open)], installable = False, runnable = False)
+        super(IsItDown, self).__init__([('Open', self.open)],
+                                       installable=False,
+                                       runnable=False)
 
     def open(self):
         webbrowser.open_new_tab("https://www.isitdownrightnow.com/")
@@ -115,18 +126,24 @@ class Infoga(HackingTool):
     DESCRIPTION = "Infoga is a tool gathering email accounts informations\n" \
                   "(ip, hostname, country,...) from different public source"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/m4ll0k/Infoga.git",
-        "cd infoga;sudo python setup.py install"
+        dict(cmd="git clone https://github.com/m4ll0k/Infoga.git"),
+        dict(cmd="sudo python setup.py install", cwd="infoga"),
     ]
-    RUN_COMMANDS = ["cd infoga;python infoga.py"]
+    RUN_COMMANDS = [
+        dict(cmd="python infoga.py", cwd="infoga"),
+    ]
     PROJECT_URL = "https://github.com/m4ll0k/Infoga"
 
 
 class ReconDog(HackingTool):
     TITLE = "ReconDog"
     DESCRIPTION = "ReconDog Information Gathering Suite"
-    INSTALL_COMMANDS = ["git clone https://github.com/s0md3v/ReconDog.git"]
-    RUN_COMMANDS = ["cd ReconDog;sudo python dog"]
+    INSTALL_COMMANDS = [
+        dict(cmd="git clone https://github.com/s0md3v/ReconDog.git"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="sudo python dog", cwd="ReconDog"),
+    ]
     PROJECT_URL = "https://github.com/s0md3v/ReconDog"
 
 
@@ -134,15 +151,14 @@ class Striker(HackingTool):
     TITLE = "Striker"
     DESCRIPTION = "Recon & Vulnerability Scanning Suite"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/s0md3v/Striker.git",
-        "cd Striker && pip3 install -r requirements.txt"
+        dict(cmd="git clone https://github.com/s0md3v/Striker.git"),
+        dict(cmd="pip3 install -r requirements.txt", cwd="Striker")
     ]
     PROJECT_URL = "https://github.com/s0md3v/Striker"
 
     def run(self):
         site = input("Enter Site Name (example.com) >> ")
-        os.system("cd Striker;")
-        subprocess.run(["sudo", "python3", "striker.py", site])
+        run_command(f"sudo python3 striker.py {site}", cwd="Striker")
 
 
 class SecretFinder(HackingTool):
@@ -152,13 +168,13 @@ class SecretFinder(HackingTool):
                   "and search anything on javascript files.\n\n " \
                   "Usage: python SecretFinder.py -h"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/m4ll0k/SecretFinder.git secretfinder",
-        "cd secretfinder; sudo pip3 install -r requirements.txt"
+        dict(cmd="git clone https://github.com/m4ll0k/SecretFinder.git"),
+        dict(cmd="sudo pip3 install -r requirements.txt", cwd="secretfinder"),
     ]
     PROJECT_URL = "https://github.com/m4ll0k/SecretFinder"
 
     def __init__(self):
-        super(SecretFinder, self).__init__(runnable = False)
+        super(SecretFinder, self).__init__(runnable=False)
 
 
 class Shodan(HackingTool):
@@ -166,11 +182,13 @@ class Shodan(HackingTool):
     DESCRIPTION = "Get ports, vulnerabilities, informations, banners,..etc \n " \
                   "for any IP with Shodan (no apikey! no rate limit!)\n" \
                   "[X] Don't use this tool because your ip will be blocked by Shodan!"
-    INSTALL_COMMANDS = ["git clone https://github.com/m4ll0k/Shodanfy.py.git"]
+    INSTALL_COMMANDS = [
+        dict(cmd="git clone https://github.com/m4ll0k/Shodanfy.py.git"),
+    ]
     PROJECT_URL = "https://github.com/m4ll0k/Shodanfy.py"
 
     def __init__(self):
-        super(Shodan, self).__init__(runnable = False)
+        super(Shodan, self).__init__(runnable=False)
 
 
 class PortScannerRanger(HackingTool):
@@ -178,24 +196,26 @@ class PortScannerRanger(HackingTool):
     DESCRIPTION = "rang3r is a python script which scans in multi thread\n " \
                   "all alive hosts within your range that you specify."
     INSTALL_COMMANDS = [
-        "git clone https://github.com/floriankunushevci/rang3r.git;"
-        "sudo pip install termcolor"]
+        dict(cmd="git clone https://github.com/floriankunushevci/rang3r.git"),
+        dict(cmd="sudo pip install termcolor"),
+    ]
     PROJECT_URL = "https://github.com/floriankunushevci/rang3r"
 
     def run(self):
         ip = input("Enter Ip >> ")
-        os.system("cd rang3r;")
-        subprocess.run(["sudo", "python", "rang3r.py", "--ip", ip])
+        run_command(f"sudo python rang3r.py --ip {ip}", cwd="rang3r")
 
 
 class Breacher(HackingTool):
     TITLE = "Breacher"
     DESCRIPTION = "An advanced multithreaded admin panel finder written in python."
-    INSTALL_COMMANDS = ["git clone https://github.com/s0md3v/Breacher.git"]
+    INSTALL_COMMANDS = [
+        dict(cmd="git clone https://github.com/s0md3v/Breacher.git"),
+    ]
     PROJECT_URL = "https://github.com/s0md3v/Breacher"
 
     def __init__(self):
-        super(Breacher, self).__init__(runnable = False)
+        super(Breacher, self).__init__(runnable=False)
 
 
 class InformationGatheringTools(HackingToolsCollection):
