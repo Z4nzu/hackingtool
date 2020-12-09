c5e4b44371739e960a7d4b44de0d6e7b74d7b923
d3v53c <shitfi63@gmail.com>
2020-12-09 09:36:43 -0800

    changes in phishing attack

--- tools/phising_attack.py
+++ tools/phising_attack.py
@@ -3,6 +3,7 @@ import os
 
 from core import HackingTool
 from core import HackingToolsCollection
+from core.utils import run_command
 
 
 class Setoolkit(HackingTool):
@@ -10,10 +11,15 @@ class Setoolkit(HackingTool):
     DESCRIPTION = "The Social-Engineer Toolkit is an open-source penetration\n" \
                   "testing framework designed for social engineering"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/trustedsec/social-engineer-toolkit.git",
-        "sudo python social-engineer-toolkit/setup.py"
+        dict(
+            cmd=
+            "git clone https://github.com/trustedsec/social-engineer-toolkit.git"
+        ),
+        dict(cmd="sudo python social-engineer-toolkit/setup.py"),
     ]
-    RUN_COMMANDS = ["sudo setoolkit"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo setoolkit"),
+    ]
     PROJECT_URL = "https://github.com/trustedsec/social-engineer-toolkit"
 
 
@@ -21,10 +27,14 @@ class SocialFish(HackingTool):
     TITLE = "SocialFish"
     DESCRIPTION = "Automated Phishing Tool & Information Collector"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/UndeadSec/SocialFish.git && sudo apt-get install python3 python3-pip python3-dev -y",
-        "cd SocialFish && sudo python3 -m pip install -r requirements.txt"
+        dict(cmd="sudo git clone https://github.com/UndeadSec/SocialFish.git"),
+        dict(cmd="sudo apt-get install python3 python3-pip python3-dev -y"),
+        dict(cmd="sudo python3 -m pip install -r requirements.txt",
+             cwd="SocialFish"),
     ]
-    RUN_COMMANDS = ["cd SocialFish && sudo python3 SocialFish.py root pass"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo python3 SocialFish.py root pass", cwd="SocialFish"),
+    ]
     PROJECT_URL = "https://github.com/UndeadSec/SocialFish"
 
 
@@ -34,10 +44,27 @@ class HiddenEye(HackingTool):
                   "Multiple Tunnelling Services \n" \
                   "\t [!]https://github.com/DarkSecDevelopers/HiddenEye"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/DarkSecDevelopers/HiddenEye.git ;sudo chmod 777 HiddenEye",
-        "cd HiddenEye;sudo pip3 install -r requirements.txt;sudo pip3 install requests;pip3 install pyngrok"
+        dict(
+            cmd=
+            "sudo git clone https://github.com/DarkSecDevelopers/HiddenEye.git"
+        ),
+        dict(cmd="sudo chmod 777 HiddenEye"),
+        dict(
+            cmd="sudo pip3 install -r requirements.txt",
+            cwd="HiddenEye",
+        ),
+        dict(
+            cmd="sudo pip3 install requests",
+            cwd="HiddenEye",
+        ),
+        dict(
+            cmd="pip3 install pyngrok",
+            cwd="HiddenEye",
+        ),
     ]
-    RUN_COMMANDS = ["cd HiddenEye;sudo python3 HiddenEye.py"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo python3 HiddenEye.py", cwd="HiddenEye"),
+    ]
     PROJECT_URL = "https://github.com/DarkSecDevelopers/HiddenEye"
 
 
@@ -52,11 +79,24 @@ class Evilginx2(HackingTool):
                   "[+]export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin \n" \
                   "[+]Then load it with source ~/.profiles."
     INSTALL_COMMANDS = [
-        "sudo apt-get install git make;go get -u github.com/kgretzky/evilginx2",
-        "cd $GOPATH/src/github.com/kgretzky/evilginx2;make",
-        "sudo make install;sudo evilginx"
+        dict(cmd="sudo apt-get install git make"),
+        dict(cmd="go get -u github.com/kgretzky/evilginx2"),
+        dict(
+            cmd="make",
+            cwd=f"${os.environ['GOPATH']}/src/github.com/kgretzky/evilginx2",
+        ),
+        dict(
+            cmd="sudo make install",
+            cwd=f"${os.environ['GOPATH']}/src/github.com/kgretzky/evilginx2",
+        ),
+        dict(
+            cmd="sudo evilginx",
+            cwd=f"${os.environ['GOPATH']}/src/github.com/kgretzky/evilginx2",
+        ),
     ]
-    RUN_COMMANDS = ["sudo evilginx"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo evilginx"),
+    ]
     PROJECT_URL = "https://github.com/kgretzky/evilginx2"
 
 
@@ -67,18 +107,25 @@ class ISeeYou(HackingTool):
                   "[!] Users can expose their local servers to the Internet " \
                   "and decode the location coordinates by looking at the log file"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/Viralmaniar/I-See-You.git",
-        "cd I-See-You && sudo chmod u+x ISeeYou.sh"
+        dict(
+            cmd="sudo git clone https://github.com/Viralmaniar/I-See-You.git"),
+        dict(cmd="sudo chmod u+x ISeeYou.sh", cwd="I-See-You"),
     ]
-    RUN_COMMANDS = ["cd I-See-You && sudo bash ISeeYou.sh"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash ISeeYou.sh", cwd="I-See-You"),
+    ]
     PROJECT_URL = "https://github.com/Viralmaniar/I-See-You"
 
 
 class SayCheese(HackingTool):
     TITLE = "SayCheese (Grab target's Webcam Shots)"
     DESCRIPTION = "Take webcam shots from target just sending a malicious link"
-    INSTALL_COMMANDS = ["sudo git clone https://github.com/hangetzzu/saycheese"]
-    RUN_COMMANDS = ["cd saycheese && sudo bash saycheese.sh"]
+    INSTALL_COMMANDS = [
+        dict(cmd="sudo git clone https://github.com/hangetzzu/saycheese"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash saycheese.sh", cwd="saycheese"),
+    ]
     PROJECT_URL = "https://github.com/hangetzzu/saycheese"
 
 
@@ -86,33 +133,44 @@ class QRJacking(HackingTool):
     TITLE = "QR Code Jacking"
     DESCRIPTION = "QR Code Jacking (Any Website)"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/cryptedwolf/ohmyqr.git && sudo apt-get install scrot"]
-    RUN_COMMANDS = ["cd ohmyqr && sudo bash ohmyqr.sh"]
+        dict(cmd="sudo git clone https://github.com/cryptedwolf/ohmyqr.git"),
+        dict(cmd="sudo apt-get install scrot"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash ohmyqr.sh", cwd="ohmyqr"),
+    ]
     PROJECT_URL = "https://github.com/cryptedwolf/ohmyqr"
 
 
 class ShellPhish(HackingTool):
     TITLE = "ShellPhish"
     DESCRIPTION = "Fhishing Tool for 18 social media"
-    INSTALL_COMMANDS = ["git clone https://github.com/An0nUD4Y/shellphish.git"]
-    RUN_COMMANDS = ["cd shellphish;sudo bash shellphish.sh"]
+    INSTALL_COMMANDS = [
+        dict(cmd="git clone https://github.com/An0nUD4Y/shellphish.git"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash shellphish.sh", cwd="shellphish"),
+    ]
     PROJECT_URL = "https://github.com/An0nUD4Y/shellphish"
 
 
 class BlackPhish(HackingTool):
     TITLE = "BlackPhish"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/iinc0gnit0/BlackPhish.git",
-        "cd BlackPhish;sudo bash install.sh"
+        dict(
+            cmd="sudo git clone https://github.com/iinc0gnit0/BlackPhish.git"),
+        dict(cmd="sudo bash install.sh", cwd="BlackPhish"),
     ]
-    RUN_COMMANDS = ["cd BlackPhish;sudo python3 blackphish.py"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo python3 blackphish.py", cwd="BlackPhish"),
+    ]
     PROJECT_URL = "https://github.com/iinc0gnit0/BlackPhish"
 
     def __init__(self):
         super(BlackPhish, self).__init__([('Update', self.update)])
 
     def update(self):
-        os.system("cd BlackPhish;sudo bash update.sh")
+        run_command("sudo bash update.sh", cwd="BlackPhish")
 
 
 class PhishingAttackTools(HackingToolsCollection):
