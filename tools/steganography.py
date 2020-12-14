d6f5c5c027a1c774496874c70bea68ddb4f92e35
d3v53c <shitfi63@gmail.com>
2020-12-14 01:20:28 -0800

    changes in steganography

--- tools/steganography.py
+++ tools/steganography.py
@@ -4,18 +4,17 @@ import subprocess
 from core import HackingTool
 from core import HackingToolsCollection
 from core import validate_input
+from core.utils import run_command
 
 
 class SteganoHide(HackingTool):
     TITLE = "SteganoHide"
-    INSTALL_COMMANDS = ["sudo apt-get install steghide -y"]
+    INSTALL_COMMANDS = [
+        dict(cmd="sudo apt-get install steghide -y"),
+    ]
 
     def run(self):
-        choice_run = input(
-            "[1] Hide\n"
-            "[2] Extract\n"
-            "[99]Cancel\n"
-            ">> ")
+        choice_run = input("[1] Hide\n" "[2] Extract\n" "[99]Cancel\n" ">> ")
         choice_run = validate_input(choice_run, [1, 2, 99])
         if choice_run is None:
             print("Please choose a valid input")
@@ -27,12 +26,12 @@ class SteganoHide(HackingTool):
         if choice_run == 1:
             file_hide = input("Enter Filename you want to Embed (1.txt) >> ")
             file_to_be_hide = input("Enter Cover Filename(test.jpeg) >> ")
-            subprocess.run(
-                ["steghide", "embed", "-cf", file_to_be_hide, "-ef", file_hide])
+            run_command(
+                f"steghide embed -cf {file_to_be_hide} -ef {file_hide}")
 
         elif choice_run == "2":
             from_file = input("Enter Filename From Extract Data >> ")
-            subprocess.run(["steghide", "extract", "-sf", from_file])
+            run_command(f"steghide extract -sf {from_file}")
 
 
 class StegnoCracker(HackingTool):
@@ -40,29 +39,26 @@ class StegnoCracker(HackingTool):
     DESCRIPTION = "SteganoCracker is a tool that uncover hidden data inside " \
                   "files\n using brute-force utility"
     INSTALL_COMMANDS = [
-        "pip3 install stegcracker && pip3 install stegcracker -U --force-reinstall"]
+        "pip3 install stegcracker && pip3 install stegcracker -U --force-reinstall"
+    ]
 
     def run(self):
         filename = input("Enter Filename:- ")
         passfile = input("Enter Wordlist Filename:- ")
-        subprocess.run(["stegcracker", filename, passfile])
+        run_command(f"stegcracker {filename} {passfile}")
 
 
 class Whitespace(HackingTool):
     TITLE = "Whitespace"
     DESCRIPTION = "Use whitespace and unicode chars for steganography"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/beardog108/snow10.git",
-        "sudo chmod -R 755 snow10"
+        dict(cmd="sudo git clone https://github.com/beardog108/snow10.git"),
+        dict(cmd="sudo chmod -R 755 snow10"),
     ]
-    RUN_COMMANDS = ["cd snow10 && firefox index.html"]
+    RUN_COMMANDS = [dict(cmd="firefox index.html", cwd="snow10")]
     PROJECT_URL = "https://github.com/beardog108/snow10"
 
 
 class SteganographyTools(HackingToolsCollection):
     TITLE = "Steganograhy tools"
-    TOOLS = [
-        SteganoHide(),
-        StegnoCracker(),
-        Whitespace()
-    ]
+    TOOLS = [SteganoHide(), StegnoCracker(), Whitespace()]
