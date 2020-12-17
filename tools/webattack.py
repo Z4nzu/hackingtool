4b32aa5294863b34437357c3feb8a290bfb232c3
d3v53c <shitfi63@gmail.com>
2020-12-17 09:45:39 -0800

    changes in web attack

--- tools/webattack.py
+++ tools/webattack.py
@@ -3,14 +3,18 @@ import subprocess
 
 from core import HackingTool
 from core import HackingToolsCollection
+from core.utils import run_command
 
 
 class Web2Attack(HackingTool):
     TITLE = "Web2Attack"
     DESCRIPTION = "Web hacking framework with tools, exploits by python"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/santatic/web2attack.git"]
-    RUN_COMMANDS = ["cd web2attack && sudo bash w2aconsole"]
+        dict(cmd="sudo git clone https://github.com/santatic/web2attack.git"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash w2aconsole", cwd="web2attack"),
+    ]
     PROJECT_URL = "https://github.com/santatic/web2attack"
 
 
@@ -20,12 +24,15 @@ class Skipfish(HackingTool):
                   "security reconnaissance tool \n " \
                   "Usage: skipfish -o [FolderName] targetip/site"
     RUN_COMMANDS = [
-        "sudo skipfish -h",
-        'echo "skipfish -o [FolderName] targetip/site"|boxes -d headline | lolcat'
+        dict(cmd="sudo skipfish -h"),
+        dict(
+            cmd=
+            'echo "skipfish -o [FolderName] targetip/site"|boxes -d headline | lolcat'
+        ),
     ]
 
     def __init__(self):
-        super(Skipfish, self).__init__(installable = False)
+        super(Skipfish, self).__init__(installable=False)
 
 
 class SubDomainFinder(HackingTool):
@@ -36,11 +43,13 @@ class SubDomainFinder(HackingTool):
                   "[1] python sublist3r.py -d example.com \n" \
                   "[2] python sublist3r.py -d example.com -p 80,443"
     INSTALL_COMMANDS = [
-        "sudo pip install requests argparse dnspython",
-        "sudo git clone https://github.com/aboul3la/Sublist3r.git",
-        "cd Sublist3r && sudo pip install -r requirements.txt"
+        dict(cmd="sudo pip install requests argparse dnspython"),
+        dict(cmd="sudo git clone https://github.com/aboul3la/Sublist3r.git"),
+        dict(cmd="sudo pip install -r requirements.txt", cwd="Sublist3r"),
     ]
-    RUN_COMMANDS = ["cd Sublist3r && python sublist3r.py -h"]
+    RUN_COMMANDS = [
+        dict(cmd="python sublist3r.py -h", cwd="Sublist3r"),
+    ]
     PROJECT_URL = "https://github.com/aboul3la/Sublist3r"
 
 
@@ -49,8 +58,11 @@ class CheckURL(HackingTool):
     DESCRIPTION = "Detect evil urls that uses IDN Homograph Attack.\n\t" \
                   "[!] python3 checkURL.py --url google.com"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/UndeadSec/checkURL.git"]
-    RUN_COMMANDS = ["cd checkURL && python3 checkURL.py --help"]
+        dict(cmd="sudo git clone https://github.com/UndeadSec/checkURL.git"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="python3 checkURL.py --help", cwd="checkURL"),
+    ]
     PROJECT_URL = "https://github.com/UndeadSec/checkURL"
 
 
@@ -58,10 +70,13 @@ class Blazy(HackingTool):
     TITLE = "Blazy(Also Find ClickJacking)"
     DESCRIPTION = "Blazy is a modern login page bruteforcer"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/UltimateHackers/Blazy.git",
-        "cd Blazy && sudo pip install -r requirements.txt"
+        dict(
+            cmd="sudo git clone https://github.com/UltimateHackers/Blazy.git"),
+        dict(cmd="sudo pip install -r requirements.txt", cwd="Blazy"),
     ]
-    RUN_COMMANDS = ["cd Blazy && sudo python blazy.py"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo python blazy.py", cwd="Blazy"),
+    ]
     PROJECT_URL = "https://github.com/UltimateHackers/Blazy"
 
 
@@ -73,13 +88,13 @@ class SubDomainTakeOver(HackingTool):
                   "that has been removed or deleted.\n" \
                   "Usage:python3 takeover.py -d www.domain.com -v"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/m4ll0k/takeover.git",
-        "cd takeover;sudo python3 setup.py install"
+        dict(cmd="git clone https://github.com/m4ll0k/takeover.git"),
+        dict(cmd="sudo python3 setup.py install", cwd="takeover"),
     ]
     PROJECT_URL = "https://github.com/m4ll0k/takeover"
 
     def __init__(self):
-        super(SubDomainTakeOver, self).__init__(runnable = False)
+        super(SubDomainTakeOver, self).__init__(runnable=False)
 
 
 class Dirb(HackingTool):
@@ -89,14 +104,17 @@ class Dirb(HackingTool):
                   "It basically works by launching a dictionary based " \
                   "attack against \n a web server and analizing the response."
     INSTALL_COMMANDS = [
-        "sudo git clone https://gitlab.com/kalilinux/packages/dirb.git",
-        "cd dirb;sudo ./configure;make"
+        dict(
+            cmd="sudo git clone https://gitlab.com/kalilinux/packages/dirb.git"
+        ),
+        dict(cmd="sudo ./configure", cwd="dirb"),
+        dict(cmd="make", cwd="dirb"),
     ]
     PROJECT_URL = "https://gitlab.com/kalilinux/packages/dirb"
 
     def run(self):
         uinput = input("Enter Url >> ")
-        subprocess.run(["sudo", "dirb", uinput])
+        run_command(f"sudo dirb {uinput}")
 
 
 class WebAttackTools(HackingToolsCollection):
