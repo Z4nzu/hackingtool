0241d265788e894a8025e2499aac5ca437ca0233
d3v53c <shitfi63@gmail.com>
2020-12-17 12:36:02 -0800

    changes in xss attack

--- tools/xss_attack.py
+++ tools/xss_attack.py
@@ -4,19 +4,23 @@ import subprocess
 
 from core import HackingTool
 from core import HackingToolsCollection
+from core.utils import run_command
 
 
 class Dalfox(HackingTool):
     TITLE = "DalFox(Finder of XSS)"
     DESCRIPTION = "XSS Scanning and Parameter Analysis tool."
     INSTALL_COMMANDS = [
-        "sudo apt-get install golang",
-        "sudo git clone https://github.com/hahwul/dalfox",
-        "cd dalfox;go install"
+        dict(cmd="sudo apt-get install golang"),
+        dict(cmd="sudo git clone https://github.com/hahwul/dalfox"),
+        dict(cmd="go install", cwd="dalfox"),
     ]
     RUN_COMMANDS = [
-        "~/go/bin/dalfox",
-        'echo "You Need To Run manually by using [!]~/go/bin/dalfox [options]"'
+        dict(cmd=f"{os.environ['HOME']}/go/bin/dalfox"),
+        dict(
+            cmd=
+            'echo "You Need To Run manually by using [!]~/go/bin/dalfox [options]"'
+        ),
     ]
     PROJECT_URL = "https://github.com/hahwul/dalfox"
 
@@ -25,10 +29,12 @@ class XSSPayloadGenerator(HackingTool):
     TITLE = "XSS Payload Generator"
     DESCRIPTION = "XSS PAYLOAD GENERATOR -XSS SCANNER-XSS DORK FINDER"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/capture0x/XSS-LOADER.git",
-        "cd XSS-LOADER;sudo pip3 install -r requirements.txt"
+        dict(cmd="git clone https://github.com/capture0x/XSS-LOADER.git"),
+        dict(cmd="sudo pip3 install -r requirements.txt", cwd="XSS-LOADER"),
     ]
-    RUN_COMMANDS = ["cd XSS-LOADER;sudo python3 payloader.py"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo python3 payloader.py", cwd="XSS-LOADER"),
+    ]
     PROJECT_URL = "https://github.com/capture0x/XSS-LOADER.git"
 
 
@@ -36,7 +42,10 @@ class XSSFinder(HackingTool):
     TITLE = "Extended XSS Searcher and Finder"
     DESCRIPTION = "Extended XSS Searcher and Finder"
     INSTALL_COMMANDS = [
-        "git glone https://github.com/Damian89/extended-xss-search.git"]
+        dict(
+            cmd="git glone https://github.com/Damian89/extended-xss-search.git"
+        ),
+    ]
     PROJECT_URL = "https://github.com/Damian89/extended-xss-search"
 
     def after_install(self):
@@ -60,44 +69,51 @@ class XSSFreak(HackingTool):
     TITLE = "XSS-Freak"
     DESCRIPTION = "XSS-Freak is an XSS scanner fully written in python3 from scratch"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/PR0PH3CY33/XSS-Freak.git",
-        "cd XSS-Freak;sudo pip3 install -r requirements.txt"
+        dict(cmd="git clone https://github.com/PR0PH3CY33/XSS-Freak.git"),
+        dict(cmd="sudo pip3 install -r requirements.txt", cwd="XSS-Freak"),
     ]
-    RUN_COMMANDS = ["cd XSS-Freak;sudo python3 XSS-Freak.py"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo python3 XSS-Freak.py", cwd="XSS-Freak"),
+    ]
     PROJECT_URL = "https://github.com/PR0PH3CY33/XSS-Freak"
 
 
 class XSpear(HackingTool):
     TITLE = "XSpear"
     DESCRIPTION = "XSpear is XSS Scanner on ruby gems"
-    INSTALL_COMMANDS = ["gem install XSpear"]
-    RUN_COMMANDS = ["XSpear -h"]
+    INSTALL_COMMANDS = [
+        dict(cmd="gem install XSpear"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="XSpear -h"),
+    ]
     PROJECT_URL = "https://github.com/hahwul/XSpear"
 
 
 class XSSCon(HackingTool):
     TITLE = "XSSCon"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/menkrep1337/XSSCon.git",
-        "sudo chmod 755 -R XSSCon"
+        dict(cmd="git clone https://github.com/menkrep1337/XSSCon.git"),
+        dict(cmd="sudo chmod 755 -R XSSCon"),
     ]
     PROJECT_URL = "https://github.com/menkrep1337/XSSCon"
 
     def run(self):
         website = input("Enter Website >> ")
-        os.system("cd XSSCon;")
-        subprocess.run(["python3", "xsscon.py", "-u", website])
+        run_command(f"python3 xsscon.py -u {website}", cwd="XSSCon")
 
 
 class XanXSS(HackingTool):
     TITLE = "XanXSS"
     DESCRIPTION = "XanXSS is a reflected XSS searching tool\n " \
                   "that creates payloads based from templates"
-    INSTALL_COMMANDS = ["git clone https://github.com/Ekultek/XanXSS.git"]
+    INSTALL_COMMANDS = [
+        dict(cmd="git clone https://github.com/Ekultek/XanXSS.git"),
+    ]
     PROJECT_URL = "https://github.com/Ekultek/XanXSS"
 
     def run(self):
-        os.system("cd XanXSS ;python xanxss.py -h")
+        run_command("python xanxss.py -h", cwd="XanXSS")
         print("\033[96m You Have to run it manually By Using\n"
               " [!]python xanxss.py [Options]")
 
@@ -106,14 +122,14 @@ class XSSStrike(HackingTool):
     TITLE = "Advanced XSS Detection Suite"
     DESCRIPTION = "XSStrike is a python script designed to detect and exploit XSS vulnerabilites."
     INSTALL_COMMANDS = [
-        "sudo rm -rf XSStrike",
-        "git clone https://github.com/UltimateHackers/XSStrike.git "
-        "&& cd XSStrike && pip install -r requirements.txt"
+        dict(cmd="sudo rm -rf XSStrike"),
+        dict(cmd="git clone https://github.com/UltimateHackers/XSStrike.git"),
+        dict(cmd="pip install -r requirements.txt", cwd="XSStrike"),
     ]
     PROJECT_URL = "https://github.com/UltimateHackers/XSStrike"
 
     def __init__(self):
-        super(XSSStrike, self).__init__(runnable = False)
+        super(XSSStrike, self).__init__(runnable=False)
 
 
 class RVuln(HackingTool):
@@ -121,12 +137,17 @@ class RVuln(HackingTool):
     DESCRIPTION = "RVuln is multi-threaded and Automated Web Vulnerability " \
                   "Scanner written in Rust"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/iinc0gnit0/RVuln.git;"
-        "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;"
-        "source $HOME/.cargo/env",
-        "sudo apt install openssl-dev;sudo cp -r RVuln/ /usr/bin"
+        dict(cmd="sudo git clone https://github.com/iinc0gnit0/RVuln.git"),
+        dict(
+            cmd=
+            "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;"),
+        dict(cmd="source $HOME/.cargo/env"),
+        dict(cmd="sudo apt install openssl-dev"),
+        dict(cmd="sudo cp -r RVuln/ /usr/bin"),
     ]
-    RUN_COMMANDS = ["RVuln"]
+    RUN_COMMANDS = [
+        dict(cmd="RVuln"),
+    ]
     PROJECT_URL = "https://github.com/iinc0gnit0/RVuln"
 
 
