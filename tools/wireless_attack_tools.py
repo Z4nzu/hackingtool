9b2cb314f8e273e429b72d1d8df145f17eab1427
d3v53c <shitfi63@gmail.com>
2020-12-17 12:16:57 -0800

    changes wireless attack tools

--- tools/wireless_attack_tools.py
+++ tools/wireless_attack_tools.py
@@ -3,6 +3,7 @@ import os
 
 from core import HackingTool
 from core import HackingToolsCollection
+from core.utils import run_command
 
 
 class WIFIPumpkin(HackingTool):
@@ -12,13 +13,15 @@ class WIFIPumpkin(HackingTool):
                   "all while forwarding legitimate traffic to and from the " \
                   "unsuspecting target."
     INSTALL_COMMANDS = [
-        "sudo apt install libssl-dev libffi-dev build-essential",
-        "sudo git clone https://github.com/P0cL4bs/wifipumpkin3.git",
-        "chmod -R 755 wifipumpkin3 && cd wifipumpkin3",
-        "sudo apt install python3-pyqt5",
-        "sudo python3 setup.py install"
+        dict(cmd="sudo apt install libssl-dev libffi-dev build-essential"),
+        dict(cmd="sudo git clone https://github.com/P0cL4bs/wifipumpkin3.git"),
+        dict(cmd="chmod -R 755 wifipumpkin3 && cd wifipumpkin3"),
+        dict(cmd="sudo apt install python3-pyqt5"),
+        dict(cmd="sudo python3 setup.py install"),
     ]
-    RUN_COMMANDS = ["sudo wifipumpkin3"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo wifipumpkin3"),
+    ]
     PROJECT_URL = "https://github.com/P0cL4bs/wifipumpkin3"
 
 
@@ -29,14 +32,16 @@ class pixiewps(HackingTool):
                   "exploiting the low or non-existing entropy of some Access " \
                   "Points, the so-called pixie dust attack"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/wiire/pixiewps.git && apt-get -y install build-essential",
-        "cd pixiewps*/ && make",
-        "cd pixiewps*/ && sudo make install && wget https://pastebin.com/y9Dk1Wjh"
+        dict(cmd="sudo git clone https://github.com/wiire/pixiewps.git"),
+        dict(cmd="sudo apt-get -y install build-essential"),
+        dict(cmd="make", cwd="pixiewps"),
+        dict(cmd="sudo make install", cwd="pixiewps"),
+        dict(cmd="wget https://pastebin.com/y9Dk1Wjh", cwd="pixiewps"),
     ]
     PROJECT_URL = "https://github.com/wiire/pixiewps"
 
     def run(self):
-        os.system(
+        run_command(
             'echo "'
             '1.> Put your interface into monitor mode using '
             '\'airmon-ng start {wireless interface}\n'
@@ -53,10 +58,16 @@ class BluePot(HackingTool):
                   "You must install/libbluetooth-dev on " \
                   "Ubuntu/bluez-libs-devel on Fedora/bluez-devel on openSUSE"
     INSTALL_COMMANDS = [
-        "wget https://github.com/andrewmichaelsmith/bluepot/raw/master/bin/bluepot-0.1.tar.gz "
-        "&& tar xfz bluepot-0.1.tar.gz && sudo java -jar bluepot/BluePot-0.1.jar"
+        dict(
+            cmd=
+            "wget https://github.com/andrewmichaelsmith/bluepot/raw/master/bin/bluepot-0.1.tar.gz"
+        ),
+        dict(cmd="tar xfz bluepot-0.1.tar.gz"),
+        dict(cmd="sudo java -jar bluepot/BluePot-0.1.jar"),
     ]
-    RUN_COMMANDS = ["cd bluepot-0.1 && sudo java -jar bluepot/BluePot-0.1.jar"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo java -jar bluepot/BluePot-0.1.jar", cwd="bluepot-0.1"),
+    ]
     PROJECT_URL = "https://github.com/andrewmichaelsmith/bluepot"
 
 
@@ -65,11 +76,14 @@ class Fluxion(HackingTool):
     DESCRIPTION = "Fluxion is a wifi key cracker using evil twin attack..\n" \
                   "you need a wireless adaptor for this tool"
     INSTALL_COMMANDS = [
-        "git clone https://github.com/thehackingsage/Fluxion.git",
-        "cd Fluxion && cd install && sudo chmod +x install.sh && sudo bash install.sh",
-        "cd .. ; sudo chmod +x fluxion.sh"
+        dict(cmd="git clone https://github.com/thehackingsage/Fluxion.git"),
+        dict(cmd="sudo chmod +x install.sh", cwd="Fluxion/install"),
+        dict(cmd="sudo bash install.sh", cwd="Fluxion/install"),
+        dict(cmd="sudo chmod +x fluxion.sh", cwd="Fluxion"),
     ]
-    RUN_COMMANDS = ["cd Fluxion;sudo bash fluxion.sh"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash fluxion.sh", cwd="Fluxion"),
+    ]
     PROJECT_URL = "https://github.com/thehackingsage/Fluxion"
 
 
@@ -84,21 +98,25 @@ class Wifiphisher(HackingTool):
         For More Details Visit >> https://github.com/wifiphisher/wifiphisher
     """
     INSTALL_COMMANDS = [
-        "git clone https://github.com/wifiphisher/wifiphisher.git",
-        "cd wifiphisher;sudo python3 setup.py install"
+        dict(cmd="git clone https://github.com/wifiphisher/wifiphisher.git"),
+        dict(cmd="sudo python3 setup.py install", cwd="wifiphisher"),
     ]
-    RUN_COMMANDS = ["cd wifiphisher;sudo wifiphisher"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo wifiphisher", cwd="wifiphisher"),
+    ]
     PROJECT_URL = "https://github.com/wifiphisher/wifiphisher"
 
 
 class Wifite(HackingTool):
     TITLE = "Wifite"
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/derv82/wifite2.git",
-        "cd wifite2 && sudo python3 setup.py install"
-        ";sudo pip3 install -r requirements.txt"
+        dict(cmd="sudo git clone https://github.com/derv82/wifite2.git"),
+        dict(cmd="sudo python3 setup.py install", cwd="wifite2"),
+        dict(cmd="sudo pip3 install -r requirements.txt", cwd="wifite2"),
     ]
-    RUN_COMMANDS = ["cd wifite2; sudo wifite"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo wifite", cwd="wifite2"),
+    ]
     PROJECT_URL = "https://github.com/derv82/wifite2"
 
 
@@ -106,8 +124,12 @@ class EvilTwin(HackingTool):
     TITLE = "EvilTwin"
     DESCRIPTION = "Fakeap is a script to perform Evil Twin Attack, by getting" \
                   " credentials using a Fake page and Fake Access Point"
-    INSTALL_COMMANDS = ["sudo git clone https://github.com/Z4nzu/fakeap.git"]
-    RUN_COMMANDS = ["cd fakeap && sudo bash fakeap.sh"]
+    INSTALL_COMMANDS = [
+        dict(cmd="sudo git clone https://github.com/Z4nzu/fakeap.git"),
+    ]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash fakeap.sh", cwd="fakeap"),
+    ]
     PROJECT_URL = "https://github.com/Z4nzu/fakeap"
 
 
@@ -117,10 +139,13 @@ class Fastssh(HackingTool):
                   " \n and brute force attack against SSH protocol using the " \
                   "most commonly credentials."
     INSTALL_COMMANDS = [
-        "sudo git clone https://github.com/Z4nzu/fastssh.git && cd fastssh && sudo chmod +x fastssh.sh",
-        "sudo apt-get install -y sshpass netcat"
+        dict(cmd="sudo git clone https://github.com/Z4nzu/fastssh.git"),
+        dict(cmd="sudo chmod +x fastssh.sh", cwd="fastssh"),
+        dict(cmd="sudo apt-get install -y sshpass netcat"),
     ]
-    RUN_COMMANDS = ["cd fastssh && sudo bash fastssh.sh --scan"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo bash fastssh.sh --scan", cwd="fastssh"),
+    ]
     PROJECT_URL = "https://github.com/Z4nzu/fastssh"
 
 
@@ -133,10 +158,12 @@ class Howmanypeople(HackingTool):
                   "especially on networks that you do not own. " \
                   "Please check your country's laws"
     INSTALL_COMMANDS = [
-        "sudo apt-get install tshark"
-        ";sudo pip install howmanypeoplearearound"
+        dict(cmd="sudo apt-get install tshark"),
+        dict(cmd="sudo pip install howmanypeoplearearound"),
     ]
-    RUN_COMMANDS = ["sudo howmanypeoplearearound"]
+    RUN_COMMANDS = [
+        dict(cmd="sudo howmanypeoplearearound"),
+    ]
 
 
 class WirelessAttackTools(HackingToolsCollection):
