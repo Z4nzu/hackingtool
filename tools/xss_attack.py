44af8910938791b614bf51ac6b41be8525facf8f
Greatest125 <66028078+Greatest125@users.noreply.github.com>
2020-12-19 15:15:56 -0500

    update RVuln install permissions

--- tools/xss_attack.py
+++ tools/xss_attack.py
@@ -125,7 +125,7 @@ class RVuln(HackingTool):
         "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh;"
         "source $HOME/.cargo/env;"
         "sudo apt install librust-openssl-dev;"
-        "cd RVuln;cargo build --release;mv target/release/RVuln"
+        "cd RVuln;sudo su;cargo build --release;mv target/release/RVuln"
     ]
     RUN_COMMANDS = ["RVuln"]
     PROJECT_URL = "https://github.com/iinc0gnit0/RVuln"
