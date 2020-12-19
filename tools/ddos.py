f75c2225c90e53336688c021a5da2f6de9e791c9
Greatest125 <66028078+Greatest125@users.noreply.github.com>
2020-12-19 10:40:03 -0500

    updated ufonet install for python3
    also gets kali dependencies

--- tools/ddos.py
+++ tools/ddos.py
@@ -45,7 +45,7 @@ class UFONet(HackingTool):
                   "More Usage Visit"
     INSTALL_COMMANDS = [
         "sudo git clone https://github.com/epsylon/ufonet.git",
-        "cd ufonet;sudo python setup.py install"
+        "cd ufonet;sudo python3 setup.py install;sudo pip3 install GeoIP;sudo pip3 install python-geoip;sudo pip3 install pygeoip;sudo pip3 install requests;sudo pip3 install pycrypto;sudo pip3 install pycurl;sudo pip3 install whois;sudo pip3 install scapy-python3"
     ]
     RUN_COMMANDS = ["sudo ./ufonet --gui"]
     PROJECT_URL = "https://github.com/epsylon/ufonet"
