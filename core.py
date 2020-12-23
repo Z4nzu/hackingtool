ed2c9dd9692cf4c33198c6a51b93106488df42e5
mokrunka <hunker.engineer@gmail.com>
2020-12-23 08:56:28 -0800

    fix my mistake on f-string in core.py

--- core.py
+++ core.py
@@ -72,7 +72,7 @@ class HackingTool(object):
         for index, option in enumerate(self.OPTIONS):
             print(f"[{index + 1}] {option[0]}")
         if self.PROJECT_URL:
-            print(f"[{98}] Open project page}")
+            print(f"[{98}] Open project page")
         print(f"[{99}] Back to {parent.TITLE if parent is not None else 'Exit'}")
         option_index = input("Select an option : ")
         try:
