1ea3c3b1689fa42984b91fc285cf0a02ddd704bb
Muneeb <muneebwanee@gmail.com>
2021-11-04 23:57:18 +0530

    Update ddos.py

--- tools/ddos.py
+++ tools/ddos.py
@@ -5,6 +5,29 @@ import subprocess
 from core import HackingTool
 from core import HackingToolsCollection
 
+class ddos(HackingTool):
+    TITLE ="ddos"
+    DESCRIPTION = "Best DDoS Attack Script With 36 Plus Methods." \
+                  "DDoS attacks\n\b " \
+                  "for SECURITY TESTING PURPOSES ONLY! "
+    
+    INSTALL_COMMANDS = [
+        "git clone https://github.com/the-deepnet/ddos.git",
+        "cd ddos;sudo pip3 install -r requirements.txt"
+    ]
+    PROJECT_URL = "https://github.com/the-deepnet/ddos.git"
+
+    def run(self):
+        method = input("Enter Method >> ")
+        url = input("Enter URL >> ")
+        threads = input("Enter Threads >> ")
+        proxylist = input(" Enter ProxyList >> ")
+        multiple = input(" Enter Multiple >> ")
+        timer = input(" Enter Timer >> ")
+        os.system("cd ddos;")
+        subprocess.run([
+            "sudo", "python3 ddos", method, url, socks_type5.4.1, threads, proxylist, multiple, timer])
+
 
 class SlowLoris(HackingTool):
     TITLE = "SlowLoris"
