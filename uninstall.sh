1f05f48a94c1a4c43a62ca771ad7df2fcd858cea
Swetsen <63598027+Swetsen@users.noreply.github.com>
2022-04-01 10:28:33 +0200

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+sudo rm -rf /usr/share/doc/hackingtool
+sudo rm -rd /usr/bin/hackingtool
+sudo pip3 uninstall lolcat
+sudo apt-get uninstall -y figlet
+sudo pip3 uninstall boxes
+sudo apt-get uninstall -y boxes
+sudo pip3 uninstall flask
+sudo pip3 uninstall requests
