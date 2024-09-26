529649f0c0f088573beec3e27c2a220411c84f43
Vignesh Skanda <agvskanda@gmail.com>
2024-09-26 10:11:18 +0530

    Update hackingtool.py
    Fixed few typos in the code.

--- hackingtool.py
+++ hackingtool.py
@@ -9,12 +9,12 @@ from time import sleep
 from core import HackingToolsCollection
 from tools.anonsurf import AnonSurfTools
 from tools.ddos import DDOSTools
-from tools.exploit_frameworks import ExploitFrameworkTools
+from tools.exploit_frameworks import ExploitFrameworksTools
 from tools.forensic_tools import ForensicTools
 from tools.information_gathering_tools import InformationGatheringTools
 from tools.other_tools import OtherTools
 from tools.payload_creator import PayloadCreatorTools
-from tools.phising_attack import PhishingAttackTools
+from tools.phishing_attack import PhishingAttackTools
 from tools.post_exploitation import PostExploitationTools
 from tools.remote_administration import RemoteAdministrationTools
 from tools.reverse_engineering import ReverseEngineeringTools
